a6c7210c6980c96efe55b9d4c3362f9e40ab5137
Yochan-moti <g19924my@gm.tsuda.ac.jp>
2021-08-04 01:21:58 +0900

    forms create

--- todo/urls.py
+++ todo/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
         path('', views.index, name='index'),  # ←一覧
         path('<int:id>/delete/', views.delete, name='delete'),  # ←削除機能用
         path('todo/<str:category>/', views.todo_category, name='todo_category'),  # ←カテゴリ
-        path('<int:id>/game/', views.encount, name='encount')
+        path('<int:id>/game/', views.encount, name='encount'),
+        path('form/', views.new, name='new'),
+        path('form/<int:pk>/', views.detail, name='detail'),
+        path('form/<int:pk>/edit/', views.edit, name='edit'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Todo, Category
+from .forms import TodoForm
+from django.utils import timezone
 
 
 """一覧表示"""
@@ -25,13 +27,55 @@ def todo_category(request, category):
     todo = Todo.objects.filter(category=category).order_by('title')
     return render(request, 'todo/index.html', {'todo': todo, 'category': category})
 
-"""ゲーム初期"""
+"""ゲーム"""
 def encount(request, id):
     todo = get_object_or_404(Todo,pk=id)
+    # obj = Todo.objects.first()
     field_name = 'level'
     field_value = getattr(todo, field_name)
     attack = int(field_value) * 100
+    brave_name = '勇者くん'
+    brave_HP = 100000
     monsterA_name = 'モンスターA'
     monsterA_HP = 10000
-    return HttpResponse(monsterA_name + " へ "+str(attack)+" の こうげき！ "
-                         + monsterA_name + " の HP は " + str(monsterA_HP-attack) + " に なった。 ")
+    monster_damageHP = monsterA_HP - attack
+    context={
+      'brave_name':brave_name,
+      'brave_HP':brave_HP,
+      'attack':attack,
+      'monster_name':monsterA_name,
+      'monster_HP':monsterA_HP,
+      'monster_damageHP':monster_damageHP,
+    }
+    return render(request, 'todo/game.html', context)
+
+def detail(request, pk):
+    todos = get_object_or_404(Todo, pk=pk)
+    return render(request, 'todo/detail.html', {'todos': todos})
+
+"""登録フォーム"""
+def new(request):
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.created_date = timezone.now()
+            todo.save()
+            return redirect('todo:detail', pk=todo.pk)
+    else:
+        form = TodoForm()
+    return render(request, 'todo/edit.html', {'form': form})
+
+"""編集フォーム"""
+def edit(request, pk):
+    todo = get_object_or_404(Todo, pk=pk)
+    if request.method == "POST":
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.created_date = timezone.now()
+            todo.save()
+            return redirect('todo:detail', pk=todo.pk)
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todo/edit.html', {'form': form})
