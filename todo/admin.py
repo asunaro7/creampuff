61d4ca0d19d6f8eb8cb8b20be933f4378928ec15
sotasota7 <75403749+sotasota7@users.noreply.github.com>
2021-08-12 19:16:07 +0900

    Update admin.py

--- todo/admin.py
+++ todo/admin.py
@@ -1,8 +1,13 @@
 # /todo/admin.py
 
 from django.contrib import admin
-from .models import Category, Todo, Game_monster
+from .models import Category, Todo, Game_monster, CharData, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower
 
 admin.site.register(Category)
 admin.site.register(Todo)
 admin.site.register(Game_monster)
+admin.site.register(CharData)
+admin.site.register(MyWeapon)
+admin.site.register(MyArmorHead)
+admin.site.register(MyArmorUpper)
+admin.site.register(MyArmorLower)
