25d732fe489ddf74f32e88998966eb45dbd16956
sotasota7 <75403749+sotasota7@users.noreply.github.com>
2021-08-12 19:14:47 +0900

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -55,3 +55,79 @@ class User(models.Model):
 
     def __str__(self):
         return self.attack
+
+"""キャラクタデータ"""
+class CharData(models.Model):
+    myName = models.CharField('名前',blank=True, null=True,default="", max_length=50)
+    myHP = models.IntegerField('HP',blank=True, null=True,default=0)
+    myLevel= models.IntegerField('レベル',blank=True, null=True,default=0)
+    myWeapon = models.CharField('武器',blank=True, null=True,default="", max_length=50)
+    myArmorHead  = models.CharField('防具　頭',blank=True, null=True,default="", max_length=50)
+    myArmorUpper  = models.CharField('防具　上',blank=True, null=True,default="", max_length=50)
+    myArmorLower  = models.CharField('防具　下',blank=True, null=True,default="", max_length=50)
+    myArmorAccessory  = models.CharField('防具　アクセサリ',blank=True, null=True,default="", max_length=50)
+    myMoney = models.IntegerField('持ち金',blank=True, null=True,default=0)
+
+    l_category = (
+      ('1', '易'),
+      ('2', 'やや易'),
+      ('3', '普通'),
+      ('4', 'やや難'),
+      ('5', '難'),
+    )
+
+    def __str__(self):
+        return self.myName
+
+"""所持武器データ"""
+class MyWeapon(models.Model):
+    myName = models.CharField('武器名',blank=True, null=True,default="", max_length=50)
+
+    attackPower  = models.IntegerField('攻撃力',blank=True, null=True,default=0)
+    defensePower  = models.IntegerField('防御力',blank=True, null=True,default=0)
+    hpRecoveryPower  = models.IntegerField('HP回復力',blank=True, null=True,default=0)
+    price  = models.IntegerField('購入額',blank=True, null=True,default=0)
+    num  = models.IntegerField('所持数',blank=True, null=True,default=0)
+
+    def __str__(self):
+        return self.myName
+
+
+"""所持防具データ"""
+class MyArmorHead(models.Model):
+    myName = models.CharField('防具名　頭',blank=True, null=True,default="", max_length=50)
+
+    attackPower  = models.IntegerField('攻撃力',blank=True, null=True,default=0)
+    defensePower  = models.IntegerField('防御力',blank=True, null=True,default=0)
+    hpRecoveryPower  = models.IntegerField('HP回復力',blank=True, null=True,default=0)
+    price  = models.IntegerField('購入額',blank=True, null=True,default=0)
+    num  = models.IntegerField('所持数',blank=True, null=True,default=0)
+
+    def __str__(self):
+        return self.myName
+
+"""所持防具データ"""
+class MyArmorUpper(models.Model):
+    myName = models.CharField('防具名　上',blank=True, null=True,default="", max_length=50)
+
+    attackPower  = models.IntegerField('攻撃力',blank=True, null=True,default=0)
+    defensePower  = models.IntegerField('防御力',blank=True, null=True,default=0)
+    hpRecoveryPower  = models.IntegerField('HP回復力',blank=True, null=True,default=0)
+    price  = models.IntegerField('購入額',blank=True, null=True,default=0)
+    num  = models.IntegerField('所持数',blank=True, null=True,default=0)
+
+    def __str__(self):
+        return self.myName
+
+"""所持防具データ"""
+class MyArmorLower(models.Model):
+    myName = models.CharField('防具名　下',blank=True, null=True,default="", max_length=50)
+
+    attackPower  = models.IntegerField('攻撃力',blank=True, null=True,default=0)
+    defensePower  = models.IntegerField('防御力',blank=True, null=True,default=0)
+    hpRecoveryPower  = models.IntegerField('HP回復力',blank=True, null=True,default=0)
+    price  = models.IntegerField('購入額',blank=True, null=True,default=0)
+    num  = models.IntegerField('所持数',blank=True, null=True,default=0)
+
+    def __str__(self):
+        return self.myName
