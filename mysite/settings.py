6f8bda6a7c17b29edfa8afc20755817d70685c8a
asunaro7 <kuku81.kuku81@gmail.com>
2021-08-22 14:46:12 +0900

    Chnged image file etc.

--- mysite/settings.py
+++ mysite/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+#                'django.template.context_processors.media', # これを追加する
             ],
         },
     },
@@ -120,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#MEDIA_URL = '/media/'   # ← これ追加
+MEDIA_URL = '/'   # ← これ追加
+MEDIA_ROOT = BASE_DIR   # ← これ追加
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
