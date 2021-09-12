9cc9ea9324edbaba814f78a5597feee78e679586
Your Name <you@example.com>
2021-09-12 13:59:57 +0900

    add messages

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,8 @@
 
 from django.urls import path
 from . import views
-
+from django.conf.urls.static import static #追加
+from django.conf import settings #追加
 
 app_name = 'todo'
 
@@ -15,7 +16,14 @@ urlpatterns = [
         path('form/', views.new, name='new'),
         path('form/<int:pk>/', views.detail, name='detail'),
         path('form/<int:pk>/edit/', views.edit, name='edit'),
+#        path('buyEquipment', views.buyEquipment, name='buyEquipment'),    # ←追加
         path('buyWeapon2', views.buyWeapon2, name='buyWeapon2'),    # ←追加
         path('dispCharData', views.dispCharData, name='dispCharData'),    # ←追加
         path('changeEquipment', views.changeEquipment, name='changeEquipment'),    # ←追加
-]
+        path('<int:id>/buyWeapon/', views.buyWeapon, name='buyWeapon'),    # ←追加
+        path('<int:id>/weaponSelect/', views.weaponSelect, name='weaponSelect'),    # ←追加
+        path('<int:id>/armorHeadSelect/', views.armorHeadSelect, name='armorHeadSelect'),    # ←追加
+        path('<int:id>/armorUpperSelect/', views.armorUpperSelect, name='armorUpperSelect'),    # ←追加
+        path('<int:id>/armorLowerSelect/', views.armorLowerSelect, name='armorLowerSelect'),    # ←追加
+        path('changeComplete', views.changeComplete, name='changeComplete'),    # ←追加
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #追加
