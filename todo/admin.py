9f6d0a57976eed1511fa2b76ccc476853690e3a8
sotasota7 <75403749+sotasota7@users.noreply.github.com>
2021-09-14 23:00:38 +0900

    Add files via upload

--- todo/admin.py
+++ todo/admin.py
@@ -1,19 +1,7 @@
 # /todo/admin.py
 
 from django.contrib import admin
-<<<<<<< HEAD
 from .models import Category, Todo, Game_monster, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower,User,User2
-=======
-<<<<<<< HEAD
-from .models import Category, Todo, Game_monster, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower,User,User2
-=======
-<<<<<<< HEAD
-from .models import Category, Todo, Game_monster, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower,User,User2
-=======
-from .models import Category, Todo, Game_monster, CharData, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower, CharData2
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 
 admin.site.register(Category)
 admin.site.register(Todo)
@@ -22,19 +10,5 @@ admin.site.register(MyWeapon)
 admin.site.register(MyArmorHead)
 admin.site.register(MyArmorUpper)
 admin.site.register(MyArmorLower)
-<<<<<<< HEAD
 admin.site.register(User)
 admin.site.register(User2)
-=======
-<<<<<<< HEAD
-admin.site.register(User)
-admin.site.register(User2)
-=======
-<<<<<<< HEAD
-admin.site.register(User)
-admin.site.register(User2)
-=======
-admin.site.register(CharData2)
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
--- todo/views.py
+++ todo/views.py
@@ -1,19 +1,7 @@
 # /todo/views.py
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-<<<<<<< HEAD
 from .models import Todo, Category, Game_monster, User, User2, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower
-=======
-<<<<<<< HEAD
-from .models import Todo, Category, Game_monster, User, User2, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower
-=======
-<<<<<<< HEAD
-from .models import Todo, Category, Game_monster, User, User2, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower
-=======
-from .models import Todo, Category, Game_monster, User, CharData, CharData2, MyWeapon, MyArmorHead, MyArmorUpper, MyArmorLower
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 from .forms import TodoForm
 from django.utils import timezone
 
@@ -107,61 +95,8 @@ def buyEquipment(request):
 
 """武器購入画面表示"""
 def buyWeapon2(request):
-#    todo = Todo.objects.order_by('title')
-    weapon = MyWeapon.objects.order_by('myName')
-    return render(request, 'todo/buyWeapon.html', {'weapon': weapon})
-
-"""装備購入（武器）"""
-def buyWeapon(request, id):
-    from .models import User2
-    weapon = get_object_or_404(MyWeapon,pk=id)
-    user = User.objects.first()
-
-    weapon.num += 1
-    user.money -= weapon.price
-    user.save()
-    weapon.save()
-
-
-
-#    user2 = User2.objects.first()
-#    if weapon.num >= 1:
-#        user2.weapon = weapon
-#        user2.save()
-
-#    wpn = MyWeapon.objects.order_by('myName')
-#    amrH = MyArmorHead.objects.order_by('myName')
-#    amrU = MyArmorUpper.objects.order_by('myName')
-#    amrL = MyArmorLower.objects.order_by('myName')
-
-#    return render(request, 'todo/buyWeapon.html', {'user': user,'user2': user2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
-    return redirect('todo:buyEquipment')
-
-"""装備購入（武器）"""
-def buyWeapon(request, id):
-    from .models import User2
-    weapon = get_object_or_404(MyWeapon,pk=id)
-    user = User.objects.first()
-
-    weapon.num += 1
-    user.money -= weapon.price
-    user.save()
-    weapon.save()
-
-
-
-#    user2 = User2.objects.first()
-#    if weapon.num >= 1:
-#        user2.weapon = weapon
-#        user2.save()
-
-#    wpn = MyWeapon.objects.order_by('myName')
-#    amrH = MyArmorHead.objects.order_by('myName')
-#    amrU = MyArmorUpper.objects.order_by('myName')
-#    amrL = MyArmorLower.objects.order_by('myName')
-
-#    return render(request, 'todo/buyWeapon.html', {'user': user,'user2': user2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
-    return redirect('todo:buyEquipment')
+    todo = Todo.objects.order_by('title')
+    return render(request, 'todo/buyWeapon.html', {'todo': todo})
 
 """装備購入（武器）"""
 def buyWeapon(request, id):
@@ -196,13 +131,6 @@ def dispCharData(request):
 
 """装備変更表示"""
 def changeEquipment(request):
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
->>>>>>> origin/master
 #    user = User.objects.order_by('brave_name')
 #    user2 = User2.objects.order_by('brave_name')
     user11 = User.objects.first()
@@ -224,46 +152,11 @@ def changeEquipment(request):
         user12.armorUpper = user11.armorUpper
         user12.armorLower = user11.armorLower
         user12.save()
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    cData = CharData.objects.order_by('myName')
-    cData2 = CharData2.objects.order_by('myName')
-    cData11 = CharData.objects.get(pk=1)
-    print(CharData2.objects.count())
-    if CharData2.objects.count() >= 1:
-        cData12 = CharData2.objects.get(pk=1)
-        cData12.myName = cData11.myName
-        cData12.myWeapon = cData11.myWeapon
-        cData12.myArmorHead = cData11.myArmorHead
-        cData12.myArmorUpper = cData11.myArmorUpper
-        cData12.myArmorLower = cData11.myArmorLower
-        cData12.save()
-    else:
-        cData12 = CharData2.objects.create(id=1,myName="tairako",myArmorHead_id = 2,myArmorUpper_id = 2,myArmorLower_id = 2,myWeapon_id = 2)
-        cData12.myName = cData11.myName
-        cData12.myWeapon = cData11.myWeapon
-        cData12.myArmorHead = cData11.myArmorHead
-        cData12.myArmorUpper = cData11.myArmorUpper
-        cData12.myArmorLower = cData11.myArmorLower
-        cData12.save()
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 
     wpn = MyWeapon.objects.order_by('myName')
     amrH = MyArmorHead.objects.order_by('myName')
     amrU = MyArmorUpper.objects.order_by('myName')
     amrL = MyArmorLower.objects.order_by('myName')
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
->>>>>>> origin/master
     return render(request, 'todo/changeEquipment.html', {'user': user11,'user2': user12, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
 
 """装備変更選択（武器）"""
@@ -275,38 +168,12 @@ def weaponSelect(request, id):
     if weapon.num >= 1:
         user2.weapon = weapon
         user2.save()
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    return render(request, 'todo/changeEquipment.html', {'charData': cData11,'charData2': cData12, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
-
-"""装備変更選択（武器）"""
-def weaponSelect(request, id):
-    from .models import CharData2
-    weapon = get_object_or_404(MyWeapon,pk=id)
-    cData = CharData.objects.get(pk=1)
-    cData2 = CharData2.objects.get(pk=1)
-    if weapon.num >= 1:
-        cData2.myWeapon = weapon
-        cData2.save()
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 
     wpn = MyWeapon.objects.order_by('myName')
     amrH = MyArmorHead.objects.order_by('myName')
     amrU = MyArmorUpper.objects.order_by('myName')
     amrL = MyArmorLower.objects.order_by('myName')
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
->>>>>>> origin/master
     return render(request, 'todo/changeEquipment.html', {'user': user,'user2': user2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
 #    return redirect('todo:changeEquipment')
 
@@ -319,38 +186,12 @@ def armorHeadSelect(request, id):
     if armorHead.num >= 1:
         user2.armorHead = armorHead
         user2.save()
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    return render(request, 'todo/changeEquipment.html', {'charData': cData,'charData2': cData2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
-
-"""装備変更選択（防具　頭）"""
-def armorHeadSelect(request, id):
-    from .models import CharData2
-    armorHead = get_object_or_404(MyArmorHead,pk=id)
-    cData = CharData.objects.get(pk=1)
-    cData2 = CharData2.objects.get(pk=1)
-    if armorHead.num >= 1:
-        cData2.myArmorHead = armorHead
-        cData2.save()
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 
     wpn = MyWeapon.objects.order_by('myName')
     amrH = MyArmorHead.objects.order_by('myName')
     amrU = MyArmorUpper.objects.order_by('myName')
     amrL = MyArmorLower.objects.order_by('myName')
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
->>>>>>> origin/master
     return render(request, 'todo/changeEquipment.html', {'user': user,'user2': user2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
 
 """装備変更選択（防具　上）"""
@@ -362,38 +203,12 @@ def armorUpperSelect(request, id):
     if armorUpper.num >= 1:
         user2.armorUpper = armorUpper
         user2.save()
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    return render(request, 'todo/changeEquipment.html', {'charData': cData,'charData2': cData2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
-
-"""装備変更選択（防具　上）"""
-def armorUpperSelect(request, id):
-    from .models import CharData2
-    armorUpper = get_object_or_404(MyArmorUpper,pk=id)
-    cData = CharData.objects.get(pk=1)
-    cData2 = CharData2.objects.get(pk=1)
-    if armorUpper.num >= 1:
-        cData2.myArmorUpper = armorUpper
-        cData2.save()
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 
     wpn = MyWeapon.objects.order_by('myName')
     amrH = MyArmorHead.objects.order_by('myName')
     amrU = MyArmorUpper.objects.order_by('myName')
     amrL = MyArmorLower.objects.order_by('myName')
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
->>>>>>> origin/master
     return render(request, 'todo/changeEquipment.html', {'user': user,'user2': user2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
 
 """装備変更選択（防具　下）"""
@@ -405,38 +220,12 @@ def armorLowerSelect(request, id):
     if armorLower.num >= 1:
         user2.armorLower = armorLower
         user2.save()
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    return render(request, 'todo/changeEquipment.html', {'charData': cData,'charData2': cData2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
-
-"""装備変更選択（防具　下）"""
-def armorLowerSelect(request, id):
-    from .models import CharData2
-    armorLower = get_object_or_404(MyArmorLower,pk=id)
-    cData = CharData.objects.get(pk=1)
-    cData2 = CharData2.objects.get(pk=1)
-    if armorLower.num >= 1:
-        cData2.myArmorLower = armorLower
-        cData2.save()
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 
     wpn = MyWeapon.objects.order_by('myName')
     amrH = MyArmorHead.objects.order_by('myName')
     amrU = MyArmorUpper.objects.order_by('myName')
     amrL = MyArmorLower.objects.order_by('myName')
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
->>>>>>> origin/master
     return render(request, 'todo/changeEquipment.html', {'user': user,'user2': user2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
 
 """装備変更完了"""
@@ -448,47 +237,12 @@ def changeComplete(request):
     user.armorUpper = user2.armorUpper
     user.armorLower = user2.armorLower
     user.save()
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    return render(request, 'todo/changeEquipment.html', {'charData': cData,'charData2': cData2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
-
-"""装備変更完了"""
-def changeComplete(request):
-    cData = CharData.objects.get(pk=1)
-    cData2 = CharData2.objects.get(pk=1)
-    cData.myWeapon = cData2.myWeapon
-    cData.myArmorHead = cData2.myArmorHead
-    cData.myArmorUpper = cData2.myArmorUpper
-    cData.myArmorLower = cData2.myArmorLower
-    cData.save()
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
 
     wpn = MyWeapon.objects.order_by('myName')
     amrH = MyArmorHead.objects.order_by('myName')
     amrU = MyArmorUpper.objects.order_by('myName')
     amrL = MyArmorLower.objects.order_by('myName')
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> origin/master
->>>>>>> origin/master
 #    return render(request, 'todo/changeEquipment.html', {'user': user,'user2': user2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
     return redirect('todo:dispCharData')
 #    return render(request)
 #    return render(request, 'todo/dispCharData.html', {'todo': todo})
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-    return render(request, 'todo/changeEquipment.html', {'charData': cData,'charData2': cData2, 'wpn': wpn, 'amrH': amrH, 'amrU': amrU, 'amrL': amrL})
->>>>>>> 2aa2e0eac509e3e97bf80cbb49944d92c5078c34
->>>>>>> origin/master
->>>>>>> origin/master
